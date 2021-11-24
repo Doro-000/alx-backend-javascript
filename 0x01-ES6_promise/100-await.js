83baec134eac3780c048c9559ae593615fdda492
Hizkyas Tekle <hizkyastekle@gmail.com>
2021-11-24 21:15:49 +0300

    advanced task

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,19 @@
+import { uploadPhoto, createUser } from '../utils';
+
+export default async function asyncUploadUser() {
+  const result = {};
+  try {
+    const upload = await uploadPhoto();
+    result.photo = upload;
+  } catch (e) {
+    result.photo = null;
+  }
+
+  try {
+    const create = await createUser();
+    result.user = create;
+  } catch (e) {
+    result.user = null;
+  }
+  return result;
+}
